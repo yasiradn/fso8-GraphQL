51b812a63e7cd8e57925260ed5d364d4d9c84151
yasir <aff3ad@gmail.com>
2020-05-02 10:46:33 +0300

    8.5

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ type Author {
   type Query{
     bookCount: Int!
     authorCount: Int!
-    allBooks(author:String!):[Book!]!
+    allBooks(genre:String!):[Book]
     allAuthors: [Author!]
 
   }
@@ -109,7 +109,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root,args) => books.filter(book => book.author === args.author),
+    allBooks: (root,args) =>  
+    books.filter(book=>book.genres.includes(args.genre)),
     allAuthors: () => authors.map(author => ({name: author.name, bookCount: books.filter( book => author.name === book.author).length}))
   }
 }
