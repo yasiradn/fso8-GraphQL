31768cdc2bcbcefc3e0705d81a7d262641b6f511
yasir <aff3ad@gmail.com>
2020-05-03 14:10:26 +0300

    8.12: Authors birth year advanced

--- library-frontend/library-frontend/src/components/Authors.js
+++ library-frontend/library-frontend/src/components/Authors.js
@@ -1,6 +1,7 @@
   
-import React from 'react'
-import { gql, useQuery } from '@apollo/client';
+import React, { useState } from 'react'
+import { gql, useMutation, useQuery, from } from '@apollo/client';
+import Select from 'react-select'
 
 const ALL_AUTHORS = gql`
 query{
@@ -11,8 +12,21 @@ query{
  }
 }
 `
+const UPDATE_AUTHOR = gql `
+mutation editAuthor($getName: String!, $byearInt: Int!) {
+  editAuthor(name: $getName, setBornTo: $byearInt)  {
+   name
+   born
+}
+}
+`
 const Authors = (props) => {
   const result_allAuthors = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState('')
+  const [byear, setByear] = useState('')
+  const [ updateAuthor ] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
   if (!props.show) {
     return null
   }
@@ -24,8 +38,17 @@ const Authors = (props) => {
   if (result_allAuthors.loading)  {
     return <div>loading...</div>
   }
-  const authors = result_allAuthors.data.allAuthors
+  let authors = result_allAuthors.data.allAuthors
+  const option_del = authors.map(op=>({label:op.name, value:op.name}))
 
+  const updateData = async (event) => {
+    event.preventDefault()
+    const byearInt = parseInt(byear)
+    const getName = name.value
+    updateAuthor({variables:{getName,byearInt}})
+    setName('')
+    setByear('')
+  }
   return (
     <div>
       <h2>authors</h2>
@@ -49,7 +72,19 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <div>
+        <form onSubmit={updateData}>
+        <Select value={name} options={option_del} onChange={(name) => setName(name)}/>
+        <div>
+        birthyear <input
+            value={byear}
+            type='number'
+            onChange={({ target }) => setByear(target.value)}
+          />
+        </div>
+           <button type='submit'>update year</button>
+        </form>
+      </div>
     </div>
   )
 }
